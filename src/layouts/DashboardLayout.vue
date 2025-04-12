7aafc52706e67a9e3c82fbfeb952d0775f16f1b0
Silas Huereca <silashuereca.dev@gmail.com>
2025-04-12 16:20:29 -0500

    chore: supabase init

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -9,20 +9,17 @@
         <nav class="mt-8">
           <ul role="list" class="flex flex-col items-center space-y-1">
             <li>
-              <!-- Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800" -->
-              <router-link to="/" class="group flex gap-x-3 rounded-md p-3 text-sm/6 font-semibold" :class="[activeRouteClass('projects-created')]">
-                <svg
-                  class="size-6 shrink-0"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                  data-slot="icon"
-                >
-                  <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                </svg>
-                <span class="sr-only">Dashboard</span>
+              <!-- Home View: will see stats here -->
+              <router-link :to="{ name: 'home'}" class="group flex gap-x-3 rounded-md p-3 text-sm/6 font-semibold" :class="[activeRouteClass('home')]">
+                <IconHome class="size-6 shrink-0" />
+                <span class="sr-only">Home</span>
+              </router-link>
+            </li>
+            <li>
+              <!-- Budgeting application -->
+              <router-link :to="{ name: 'budget'}" class="group flex gap-x-3 rounded-md p-3 text-sm/6 font-semibold" :class="[activeRouteClass('budget')]">
+                <IconCurrencyDollar class="size-6 shrink-0" />
+                <span class="sr-only">Budget</span>
               </router-link>
             </li>
           </ul>
@@ -48,6 +45,8 @@
 <script lang="ts" setup>
 import { useRoute } from "vue-router";
 
+import IconCurrencyDollar from "../icons/IconCurrencyDollar.vue";
+import IconHome from "../icons/IconHome.vue";
 import IconSettings from "../icons/IconSettings.vue";
 import { useAppStore } from "../store";
 const { state } = useAppStore();
