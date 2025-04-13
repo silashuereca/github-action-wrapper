4f64c6fec1109252c0a7c429aff5b8bc1368ef01
Silas Huereca <silashuereca.dev@gmail.com>
2025-04-13 11:38:01 -0500

    fix: type errors

--- supabase/functions/create-budget/index.ts
+++ supabase/functions/create-budget/index.ts
@@ -1,24 +1,26 @@
-import { createClient } from "jsr:@supabase/supabase-js@2";
+// import { createClient } from "jsr:@supabase/supabase-js@2";
 
-import { corsHeaders } from "../_shared/cores.ts";
+// import { corsHeaders } from "../_shared/cores.ts";
 
-Deno.serve(async (req: Request) => {
-  if (req.method === "OPTIONS") {
-    return new Response("ok", { headers: corsHeaders });
-  }
+// Deno.serve(async (req: Request) => {
+//   if (req.method === "OPTIONS") {
+//     return new Response("ok", { headers: corsHeaders });
+//   }
 
-  const supabaseClient = createClient(Deno.env.get("SUPABASE_URL") ?? "", Deno.env.get("SUPABASE_ANON_KEY") ?? "", {
-    global: {
-      headers: corsHeaders,
-    },
-  });
-  // Get the session or user object
-  const authHeader = req.headers.get("Authorization")!;
-  const token = authHeader.replace("Bearer ", "");
-  const { data } = await supabaseClient.auth.getUser(token);
-  const user = data.user;
-  return new Response(JSON.stringify({ user }), {
-    headers: { ...corsHeaders, "Content-Type": "application/json" },
-    status: 200,
-  });
-});
+//   const client: SupabaseClientOptions;
+
+//   const supabaseClient = createClient(Deno.env.get("SUPABASE_URL") ?? "", Deno.env.get("SUPABASE_ANON_KEY") ?? "", {
+//     global: {
+//       headers: corsHeaders,
+//     },
+//   });
+//   // Get the session or user object
+//   const authHeader = req.headers.get("Authorization")!;
+//   const token = authHeader.replace("Bearer ", "");
+//   const { data } = await supabaseClient.auth.getUser(token);
+//   const user = data.user;
+//   return new Response(JSON.stringify({ user }), {
+//     headers: { ...corsHeaders, "Content-Type": "application/json" },
+//     status: 200,
+//   });
+// });
